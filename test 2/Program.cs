ad1998a5134aeda35392424e64907b5110fb3e13
Oldertz <viggo.oldertz@gmail.com>
2017-09-12 10:10:05 +0200

    Ändrade texten

--- test 2/Program.cs
+++ test 2/Program.cs
@@ -6,7 +6,7 @@ namespace test_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Funkar det nudå?");
         }
     }
 }
